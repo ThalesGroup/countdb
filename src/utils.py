9e4b6ddecf01d1d343ea3ae3844596850ae7577b
orin <orin@imperva.com>
2025-08-19 11:42:14 +0300

    allow setting max workers using env

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,11 @@
+import os
 from datetime import datetime, timedelta, date
 from typing import Generator
 
 import boto3
 from boto3 import Session
 
-_MAX_WORKERS = 1
+_MAX_WORKERS = os.environ.get("MAX_WORKERS", 3)
 
 
 def get_session(session: Session = None) -> boto3.Session:
